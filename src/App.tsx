3817ab8070cb1b7df764cecda79350c253eec131
Shefat Ahmed <shefata69@gmail.com>
2025-02-12 20:33:06 +0600

    complete application

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-function App() {
-  return (
-    <div>
+import { decrement, increment, incrementByValue } from "./redux/features/counterSlice"
+import { useAppDispatch, useAppSelector } from "./redux/hooks"
 
+function App() {
+  const {count} = useAppSelector((state)=> state.counter)
+  const dispatch = useAppDispatch()
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="flex bg-amber-100 p-5 gap-5 items-center">
+        <button  onClick={() => dispatch(increment())} className="bg-green-400 p-5  rounded-3xl">Increment</button>
+        <h1>{count}</h1>
+        <button onClick={() => dispatch(decrement())} className="bg-red-400 p-5 rounded-3xl">Decrement</button>
+        <button onClick={() => dispatch(incrementByValue(5))} className="bg-red-400 p-5 rounded-3xl">IncrementBY5</button>
+      </div>
     </div>
   )
 }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit"
-
+import { configureStore } from "@reduxjs/toolkit";
+import couterReducer from "./features/counterSlice";
+import logger from "./middleware/logger";
 export const store = configureStore({
-        reducer: {}
-})
+  reducer: {
+    counter: couterReducer,
+  },
+  middleware: (getDefaultMiddleware)=> getDefaultMiddleware().concat(logger)
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
